91fa56ebe662102cfa7b9a09e15e4c0613f6cf97
Amos Stone <amos.stone@activenetwork.com>
2012-12-07 14:54:22 -0800

    updates gem for post methods.

--- lib/carepass.rb
+++ lib/carepass.rb
@@ -1,6 +1,8 @@
 require "carepass/version"
 
 module Carepass
+
+
   # test method
   def self.test
   	puts 'testing out cool'
@@ -8,29 +10,36 @@ module Carepass
 
   class User
   	attr_accessor :token
-    
+
   	def initialize (token)
       raise Error, 'access token is required' if token.nil?
       @token = token
       apikey = CAREPASS_CONFIG[:client_id]
-      @headers = {'Authorization' => "Bearer #{@token}", 
-                  'Accept' => 'application/json', 
+      @headers = {'Authorization' => "Bearer #{@token}",
+                  'Accept' => 'application/json',
                   'Apikey' => "#{apikey}"}
       self
   	end
 
     def id
-      response = HTTParty.get('https://api.carepass.com/user-directory-api/users/currentUser', :headers => @headers)
+      response = self.get('https://api.carepass.com/user-directory-api/users/currentUser')
       response['id']
-      # todo: add catch on failure
-    end    
-
-    def info
-      response = HTTParty.get('https://api.carepass.com/user-directory-api/users/currentUser', :headers => @headers)
       # todo: add catch on failure
     end
 
+    def info
+      self.get('https://api.carepass.com/user-directory-api/users/currentUser')
+      # todo: add catch on failure
+    end
 
+    def post(url, query_parameters)
+      response = HTTParty.post(url, :headers => @headers, :body => query_parameters)
+      # todo: add catch on failure
+    end
+
+    def get(url, query_parameters=nil)
+      response = HTTParty.get(url, :headers => @headers, :query => query_parameters)
+    end
   end
 
 end
