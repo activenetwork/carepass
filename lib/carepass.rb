9732ad7f53405a4cdd67bf63ce053500f95dd0bc
Amos Stone <amos.stone@activenetwork.com>
2012-12-14 16:58:51 -0800

    Implements workout post

--- lib/carepass.rb
+++ lib/carepass.rb
@@ -2,11 +2,8 @@ require "carepass/version"
 
 module Carepass
 
-
-  # test method
-  def self.test
-  	puts 'testing out cool'
-  end
+  USER_ENDPOINT = 'https://api.carepass.com/user-directory-api/users/currentUser'
+  ACTIVITY_ENDPOINT = USER_ENDPOINT + '/fitness/activities'
 
   class User
   	attr_accessor :token
@@ -22,21 +19,33 @@ module Carepass
   	end
 
     def id
-      response = self.get('https://api.carepass.com/user-directory-api/users/currentUser')
+      response = self.get(USER_ENDPOINT)
       response['id']
       # todo: add catch on failure
     end
 
     def info
-      self.get('https://api.carepass.com/user-directory-api/users/currentUser')
+      self.get(USER_ENDPOINT)
       # todo: add catch on failure
+    end
+
+    def upload_workout(query_parameters)
+      if query_parameters.first.include? :id
+        self.put(ACTIVITY_ENDPOINT, query_parameters)
+      else
+        self.post(ACTIVITY_ENDPOINT, query_parameters)
+      end
     end
 
     def post(url, query_parameters)
       post_headers = @headers
       post_headers['Content-Type'] = 'application/json'
-      response = HTTParty.post(url, :headers => post_headers, :body => query_parameters)
+      response = HTTParty.post(url, :headers => post_headers, :body => query_parameters.to_json)
       # todo: add catch on failure
+    end
+
+    def put(url, query_parameters)
+      response = HTTParty.put(url, :query => query_paramters)
     end
 
     def get(url, query_parameters=nil)
